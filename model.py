a00e406952ed1339ee72f49a7d2e0ebb7a95ff38
Sumanth <sumanth@sumanths-MacBook-Air.local>
2026-05-11 22:53:20 +0530

    Postiinal Encoding and Layer Normalisation

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn 
+import math
 
 class InputEmbeddings(nn.Module) :
     
@@ -11,6 +12,61 @@ class InputEmbeddings(nn.Module) :
      self.embedding=nn.Embedding(vocab_size,d_model)
      
      def forward(self, x) :
+         return self.embedding(x) * math.sqrt(self.d_model)
+     
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len:int, dropout:float) -> None :
+        super().__init__()
+        self.d_model=d_model
+        self.seq_len=seq_len
+        self.dropout=nn.Dropout(dropout)
+        
+        #creating a Matrix of shape (seq_len, d_model)
+        pe=torch.zeros(seq_len, d_model)
+        #creating a vector of shape (seq_len, 1)
+        position=torch.arange(0,seq_len, dtype=torch.float).unsqueeze(1)
+        div_term=torch.exp(torch.arrange(0,d_model,2).float() * (-math.log(10000.0)/d_model))
+        #apply the trig func to respective odd and even positions
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        
+        pe = pe.unsqeeze(0) #(1, seq_len, d_model)
+        
+        self.register_buffer('pe', pe)
+    
+    def forward(self, x) :
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
+
+class LayerNormalization(nn.Module) :
+    
+    def __init__(self, eps: float = 10**-6) -> None :
+        super().__init__()
+        self.eps = eps
+        self.alpha=nn.Parameter(torch.ones(1)) #Multiplied
+        self.bias=nn.Parameter(torch.zeros(1)) #Added
+    
+    def forward(self, x) :
+        mean=x.mean(dim=-1, keepdim=True)
+        std=x.std(dim=-1, keepdim=True)
+        return self.alpha * (x-mean) / (std + self.eps) + self.bias
+
+class FeedForwardBlock(nn.Module) :
+    
+    def __init__(self, d_model : int , d_ff : int, dropout: float) -> None :
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) #W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) #W2 and B2
+    
+    def forward(self, x) : 
+        # (Batch, seq_Len , d_model) --> (Batch, seq_len, d_ff) --> (Batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+        
+        
+    
      
     
     
